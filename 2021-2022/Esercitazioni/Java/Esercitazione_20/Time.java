da0ff700e47e99e612da3c2620739866723d8584
Quijada Pierpaolo <pierpaolo.quijadagomez@istitutoagnelli.it>
2021-11-26 12:18:40 +0100

    upload

---
+++ 2021-2022/Esercitazioni/Java/Esercitazione_20/Time.java
@@ -0,0 +1,58 @@
+public class Time {
+    private int ore;
+    private int minuti;
+    private int secondi;
+
+    public Time(int ore, int minuti, int secondi) {
+        this.ore = ore;
+        this.minuti = minuti;
+        this.secondi = secondi;
+    }
+
+    public void setOre(int ore_2) {
+        ore = ore_2;
+    }
+
+    public void setMinuti(int minuti_2) {
+        minuti = minuti_2;
+    }
+
+    public void setSecondi(int secondi_2) {
+        secondi = secondi_2;
+    }
+
+    public int getOre() {
+        return ore;
+    }
+
+    public int getMinuti() {
+        return minuti;
+    }
+
+    public int getSecondi() {
+        return secondi;
+    }
+
+    public void addTime(Time t) {
+        ore = ore + t.getOre();
+        minuti = minuti + t.getMinuti();
+        secondi = secondi + t.getSecondi();
+        if (secondi > 59) {
+            minuti++;
+            secondi = secondi - 60;
+        }
+        if (minuti > 59) {
+            ore++;
+            minuti = minuti - 60;
+        }
+
+        if (ore > 23)
+            ore = ore - 24;
+    }
+
+    public String toString() {
+        String s = "";
+        s = s + "Ore: " + getOre() + " \nMinuti: " + getMinuti() + " \nSecondi: " + getSecondi();
+        return s;
+    }
+}
