f656384464367b871c8dc60aeac687f19d312669
Pierpaolo Quijada Gomez <pierpaologomez05@gmail.com>
2021-12-04 11:22:54 +0100

    upload

--- 2021-2022/Esercitazioni/Java/Esercitazione_20/Main.java
+++ 2021-2022/Esercitazioni/Java/Esercitazione_20/Main.java
@@ -13,52 +13,52 @@ public class Main {
         secondi = input.nextInt();
         Time tempo = new Time(ore, minuti, secondi);
         Time tempo_2;
+        Time tempo_3;
 
         do {
             System.out.println(
                     " 1) Aggiungi orario\n 2) Sottrarre due orari\n 3) Stampa orario\n 4) Validazione\n 0) Esci");
             scelta = input.nextInt();
             switch (scelta) {
-            case 1:
-                System.out.println("Inserire le ore:");
-                ore_2 = input.nextInt();
-                System.out.println("Inserire i minuti");
-                minuti_2 = input.nextInt();
-                System.out.println("Inserire i secondi");
-                secondi_2 = input.nextInt();
-                tempo_2 = new Time(ore_2, minuti_2, secondi_2);
-                tempo.addTime(tempo_2);
-                break;
-
-            case 2:
-                System.out.println("Inserire le ore:");
-                ore_2 = input.nextInt();
-                System.out.println("Inserire i minuti");
-                minuti_2 = input.nextInt();
-                System.out.println("Inserire i secondi");
-                secondi_2 = input.nextInt();
-                ore = ore - ore_2;
-                minuti = minuti - minuti_2;
-                secondi = secondi - secondi_2;
-                break;
-
-            case 3:
-                System.out.println(tempo);
-                break;
-
-            case 4:
-                if (ore > 24 || minuti > 59 || secondi > 59) {
-                    System.out.println("Orario non valido");
+                case 1:
                     System.out.println("Inserire le ore:");
-                    ore = input.nextInt();
+                    ore_2 = input.nextInt();
                     System.out.println("Inserire i minuti");
-                    minuti = input.nextInt();
+                    minuti_2 = input.nextInt();
                     System.out.println("Inserire i secondi");
-                    secondi = input.nextInt();
-                } else
-                    System.out.println("Orario valido");
+                    secondi_2 = input.nextInt();
+                    tempo_2 = new Time(ore_2, minuti_2, secondi_2);
+                    tempo.addTime(tempo_2);
+                    break;
 
-                break;
+                case 2:
+                    System.out.println("Inserire le ore:");
+                    ore_2 = input.nextInt();
+                    System.out.println("Inserire i minuti");
+                    minuti_2 = input.nextInt();
+                    System.out.println("Inserire i secondi");
+                    secondi_2 = input.nextInt();
+                    tempo_3 = new Time(ore_2, minuti_2, secondi_2);
+                    tempo.subTime(tempo_3);
+                    break;
+
+                case 3:
+                    System.out.println(tempo);
+                    break;
+
+                case 4:
+                    if (ore > 24 || minuti > 59 || secondi > 59) {
+                        System.out.println("Orario non valido, inserire nuovamente i dati.");
+                        System.out.println("Inserire le ore:");
+                        ore = input.nextInt();
+                        System.out.println("Inserire i minuti");
+                        minuti = input.nextInt();
+                        System.out.println("Inserire i secondi");
+                        secondi = input.nextInt();
+                    } else
+                        System.out.println("Orario valido");
+
+                    break;
             }
         } while (scelta != 0);
     }
